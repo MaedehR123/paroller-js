3acf094fc32eda12c2585c4836de755e4375097f
maedeh <rezaei.maedeh2323@gmail.com>
2025-10-28 11:57:16 +0330

    test on phone

--- main.js
+++ main.js
@@ -1,17 +1,9 @@
-// $(".bg-paroller").paroller({
-//   factor: 0.3,
-//   factorXs: 0,
-//   type: "foreground",
-//   direction: "horizontal",
-// });
-
-
 $(".bg-paroller").paroller({
-  factor: 0.3,        // سرعت افکت در دسکتاپ
-  factorXs: 0.2,      // سرعت کمتر برای موبایل (ولی صفر نباشه!)
-  factorSm: 0.25,     // تبلت‌ها
+  factor: 0.3,
+  factorXs: 0,
   type: "foreground",
   direction: "horizontal",
-  transition: "transform 0.2s ease-out"
+  // direction: "vertical",
 });
+
 
