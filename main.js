6e7876a1c166f9cd2e35f47d85364b1dec133d68
maedeh <rezaei.maedeh2323@gmail.com>
2025-10-28 11:49:07 +0330

    fixe problem on phones

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
+// $(".bg-paroller").paroller({
+//   factor: 0.3,
+//   factorXs: 0,
+//   type: "foreground",
+//   direction: "horizontal",
+// });
+
+
 $(".bg-paroller").paroller({
-  factor: 0.3,
-  factorXs: 0,
+  factor: 0.3,        // سرعت افکت در دسکتاپ
+  factorXs: 0.2,      // سرعت کمتر برای موبایل (ولی صفر نباشه!)
+  factorSm: 0.25,     // تبلت‌ها
   type: "foreground",
   direction: "horizontal",
+  transition: "transform 0.2s ease-out"
 });
+
