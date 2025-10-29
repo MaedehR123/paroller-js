3155a8c32576161f64b310dda6515593ae6148d7
maedeh <rezaei.maedeh2323@gmail.com>
2025-10-29 10:41:01 +0330

    test 4

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 $(".bg-paroller").paroller({
   factor: 0.3,
-  factorXs: 0,
+  factorXs: 0.15,
   type: "foreground",
   direction: "horizontal",
   // direction: "vertical",
 });
 
 
+// main.js
+if (window.innerWidth > 480) {
+  $(".bg-paroller").paroller({
+    factor: 0.3,
+    type: "foreground",
+    direction: "horizontal"
+  });
+}
